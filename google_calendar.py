76058c76ca99f274dd840f69615db13d04bfce9f
Natig Babayev <hello@natigbabayev.com>
2021-04-29 10:28:13 +0300

    Code cleanup

--- google_calendar.py
+++ google_calendar.py
@@ -15,7 +15,7 @@ scheduler = BackgroundScheduler()
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 # Location where screenshots will be located when not attending at any meeting
-DEFAULT_SCREENSHOT_LOCATION = ""
+DEFAULT_SCREENSHOT_LOCATION = ''
 
 # Sleep time represented in seconds before running kilall SystemUIServer
 TIME_OFFSET = 5
@@ -30,9 +30,10 @@ def time_in_range(start, end, x):
 
 
 def scheduler_date(event_start_date, event_end_date, now, event_name):
-    new_screenshot_location = DEFAULT_SCREENSHOT_LOCATION + "/" + event_name
+    new_screenshot_location = DEFAULT_SCREENSHOT_LOCATION + '/' + event_name
     schedule_date = event_start_date - datetime.timedelta(seconds=TIME_OFFSET)
 
+    # first check if path already exists. otherwise, check if we're in middle of event
     if os.path.exists(new_screenshot_location):
         schedule_date = None
     elif time_in_range(event_start_date, event_end_date, now):
@@ -77,30 +78,30 @@ def fetch_events(start_date, end_date):
     return events_result.get('items', [])
 
 
+def run_bash_command(bashCmd):
+    process = subprocess.Popen(bashCmd, stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+    if error is not None:
+        print(error)
+
+
 def change_screenshot_location(event_name=''):
-    print("Changing screenshot location...")
-    new_screenshot_location = DEFAULT_SCREENSHOT_LOCATION + "/" + event_name
+    print('Changing screenshot location...')
+    new_screenshot_location = DEFAULT_SCREENSHOT_LOCATION + '/' + event_name
     # Create folder if doesn't exisst
     if not os.path.exists(new_screenshot_location):
         os.makedirs(new_screenshot_location)
 
     # Change screenshot location
-    bashCmd = ["defaults", "write", "com.apple.screencapture",
-               "location", "\"{}\"".format(new_screenshot_location)]
-    process = subprocess.Popen(bashCmd, stdout=subprocess.PIPE)
-    output1, error1 = process.communicate()
-
-    if error1 is not None:
-        print(error1)
+    bashCmd = ['defaults', 'write', 'com.apple.screencapture',
+               'location', '"{}"'.format(new_screenshot_location)]
+    run_bash_command(bashCmd)
 
     # We need this so that changes are applied
     time.sleep(TIME_OFFSET)
-    bashCmd2 = ["killall", "SystemUIServer"]
-    process2 = subprocess.Popen(bashCmd2, stdout=subprocess.PIPE)
-    output2, error2 = process2.communicate()
-
-    if error2 is not None:
-        print(error2)
+    bashCmd2 = ['killall', 'SystemUIServer']
+    run_bash_command(bashCmd2)
 
 
 def fetch_events_and_schedule():
@@ -117,35 +118,37 @@ def fetch_events_and_schedule():
         start = event['start'].get('dateTime', event['start'].get('date'))
         end = event['end'].get('dateTime', event['end'].get('date'))
         event_start_date = datetime.datetime.strptime(
-            start, "%Y-%m-%dT%H:%M:%S%z")
-        event_end_date = datetime.datetime.strptime(end, "%Y-%m-%dT%H:%M:%S%z")
+            start, '%Y-%m-%dT%H:%M:%S%z')
+        event_end_date = datetime.datetime.strptime(end, '%Y-%m-%dT%H:%M:%S%z')
 
         event_title = event['summary']
 
-        unique_job_id = "{}-{}-{}".format(start, end, event_title)
+        unique_job_id = '{}-{}-{}'.format(start, end, event_title)
 
-        folder_name = "{}_{}".format(
-            event_start_date.strftime("%Y%m%d%H%M"), event_title)
+        folder_name = '{}_{}'.format(
+            event_start_date.strftime('%Y%m%d%H%M'), event_title)
         possible_job = scheduler.get_job(unique_job_id)
 
         schedule_date = scheduler_date(
             event_start_date, event_end_date, datetime.datetime.now().astimezone(), folder_name)
 
         if possible_job is None and schedule_date is not None:
-            print("Scheduling job for {}".format(unique_job_id))
+            print('Scheduling job for {}'.format(unique_job_id))
 
             scheduler.add_job(lambda: change_screenshot_location(
                 folder_name), 'date', run_date=schedule_date, id=unique_job_id)
 
             # when event ends, reset screenshot folder
-            scheduler.add_job(lambda: change_screenshot_location(), 'date', run_date=event_end_date)
+            scheduler.add_job(lambda: change_screenshot_location(),
+                              'date', run_date=event_end_date)
 
         time.sleep(2)
 
-    print("==================================")
+    print('==================================')
 
 
 def main():
+    # we can already schedule job for checking if there's new events
     scheduler.add_job(fetch_events_and_schedule, 'interval',
                       seconds=15, id='fetch_events_and_schedule')
     scheduler.start()
@@ -154,9 +157,10 @@ def main():
 if __name__ == '__main__':
     main()
     try:
+        # keep the script running
         while True:
             time.sleep(2)
     except KeyboardInterrupt:
-        print("Closing Google Calendar Automation...")
-        print("Shutting down scheduler...")
+        print('Closing Google Calendar Automation...')
+        print('Shutting down scheduler...')
         scheduler.shutdown(wait=False)
